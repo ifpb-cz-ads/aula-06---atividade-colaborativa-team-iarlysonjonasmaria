118f2b170de782ae4300c501fe65ffeec2faabc5
kelcileneBispo <kelciparnaiba@gmail.com>
2020-11-01 21:58:39 -0300

    criacao do index e modificacao no servlet Auth

--- aula06/src/main/java/br/edu/ifpb/AuthServlet.java
+++ aula06/src/main/java/br/edu/ifpb/AuthServlet.java
@@ -1,15 +1,33 @@
 package br.edu.ifpb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AuthServlet extends HttpServlets {
+@WebServlet("/Auth")
+public class AuthServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
+            PrintWriter out = response.getWriter();
+
+            String name = request.getParameter("name");
+            String senha = request.getParameter("senha");
+
+            HashMap<String, String> usuario = new HashMap<String, String>( );
+            usuario.put("maria", "123maria");
+            usuario.put("pedro", "456pedro");
+            usuario.put("laura", "122laura");
+
+           out.println(usuario);
+
+            
             
         }
 }
--- aula06/target/classes/META-INF/resources/index.jsp
+++ aula06/target/classes/META-INF/resources/index.jsp
@@ -1,5 +1,23 @@
 <html>
-<body>
-<h2>Hello World!</h2>
+    <meta charset="UTF-8"> 
+    <title>Questoes</title> 
+    </head>
+    <body>
+
+        <fieldset style="background: #eeeeee;"> 
+            <legend>Questao 01 </legend>
+            <form style="text-align: center;" method="POST" action="SelectProduto.html"> 
+                <input type="submit" value="Questao01">      
+            </form> 
+        </fieldset>
+
+    <fieldset style="background: #eeeeee;"> 
+    <legend>Questao 02 </legend>
+     <form style="text-align: center;" method="GET" action="formUsuario.html"> 
+         <input type="submit" value="Questao02">
+      </form> 
+     </fieldset>
+
+
 </body>
 </html>
