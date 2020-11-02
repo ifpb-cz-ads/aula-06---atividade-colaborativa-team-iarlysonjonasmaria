6cae52531fbe6850cc9994a3533e59ebfe0a271e
kelcileneBispo <kelciparnaiba@gmail.com>
2020-11-02 15:23:34 -0300

    modificacao no autenticacao de usuario

--- aula06/src/main/java/br/edu/ifpb/AuthServlet.java
+++ aula06/src/main/java/br/edu/ifpb/AuthServlet.java
@@ -2,32 +2,50 @@ package br.edu.ifpb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.*;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Auth")
+@WebServlet(
+urlPatterns  = {"/Auth"},
+initParams = {
+    @WebInitParam(name = "Jose", value = "jose12"),
+    @WebInitParam(name = "Maria", value = "maria12"),
+    @WebInitParam(name = "Joao", value = "joao12"),
+    @WebInitParam(name = "Laura", value = "laura12"),
+}
+)
 public class AuthServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
             PrintWriter out = response.getWriter();
 
-            String name = request.getParameter("name");
+            String name = request.getParameter("texto");
             String senha = request.getParameter("senha");
 
-            HashMap<String, String> usuario = new HashMap<String, String>( );
-            usuario.put("maria", "123maria");
-            usuario.put("pedro", "456pedro");
-            usuario.put("laura", "122laura");
-
-           out.println(usuario);
-
+            ServletConfig config = this.getServletConfig();
+            HashMap<Usuario, Usuario> usuario = new HashMap<Usuario, Usuario>( );
+            Enumeration<String> paramNames = config.getInitParameterNames();
             
+
+          
+            while (paramNames.hasMoreElements()) {
+            String nome = paramNames.nextElement();
+            String value = config.getInitParameter(nome);
+            usuario.put( new Usuario(nome, config.getInitParameter(nome)), 
+            new Usuario(senha, config.getInitParameter(value)) );
+        }
+           
+            out.println(usuario);
             
         }
 }
---
+++ aula06/src/main/java/br/edu/ifpb/Usuario.java
@@ -0,0 +1,30 @@
+package br.edu.ifpb;
+
+public class Usuario {
+
+    private String nome;
+    private String senha;
+
+    public Usuario() {}
+
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
